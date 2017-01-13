04d0b7a290bc5773ecbf0092b4bacf77e1e18521
aunik <aunik_80@yahoo.com>
2017-01-13 13:37:46 +0600

    added comment activity

---
+++ app/src/main/java/com/example/aunik/roadcondition2/CommentActivity.java
@@ -0,0 +1,13 @@
+package com.example.aunik.roadcondition2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class commentActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
